02ffc5cad5f50f75df490b586ee337ec3c82a2c9
Anton <rychkin06@gmail.com>
2022-10-02 15:13:44 +0300

    v0.0.1
    Now without description.

--- screenshot.ps1
+++ screenshot.ps1
@@ -1,20 +1,14 @@
-<# :
-  @echo off
-    powershell /nop /ex bypass^
-    "&{[ScriptBlock]::Create((gc '%~f0') -join [Char]10).Invoke()}"
-  exit /b
-#>
 (New-Object -ComObject Shell.Application).MinimizeAll()
 Add-Type -AssemblyName System.Windows.Forms
 $scr = [Windows.Forms.SystemInformation]::VirtualScreen
-
+$DailyDir = $args[0]
 
 while (1) {
     $bmp = New-Object Drawing.Bitmap $scr.Width, $scr.Height
     $gfx = [Drawing.Graphics]::FromImage($bmp)
     $gfx.CopyFromScreen($scr.Location, [Drawing.Point]::Empty, $scr.Size)
     $gfx.Dispose()
-    $bmp.Save("./scrsht_$(Get-Date -Format "MM/dd/yyyy_HH-mm-ss" | ForEach-Object { $_ -replace "\.", "-" }).png")
+    $bmp.Save("$DailyDir$(Get-Date -Format 'MM/dd/yyyy_HH-mm-ss' | ForEach-Object { $_ -replace '\.', '-' }).png")
     $bmp.Dispose()
     $counter = $counter + 1
     Start-Sleep 5
